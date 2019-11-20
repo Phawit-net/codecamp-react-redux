ad0750bfe6e22f4ac5f9ad64a26568f798503dd8
Phawit-net <neztle.net@gmail.com>
2019-11-20 18:40:53 +0700

    commite

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -8,8 +8,24 @@ import Todo from './components/Todo'
 class TodoList extends React.Component {
   state = {
     textValue: '',
-    todos: [{ ticked: false, name: 'KILL DA BEECH' }]
+    todos: []
   }
+
+    componentDidMount = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/todos')
+      const data = await response.json()
+      console.log(data)
+
+      this.setState(state =>({
+        todos:data.map(x=>x)
+      }))
+      
+    } catch (err) {
+      console.log(err)
+    }
+   }
+
 
   handleChange = e => {
     this.setState({
@@ -17,8 +33,22 @@ class TodoList extends React.Component {
     })
   }
 
-  handleAdd = () => {
+  handleAdd = async() => {
     if (!this.state.textValue) return
+    try {
+    console.log(this.state.textValue)
+    await fetch('http://localhost:3001/todos', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        ticked: 'false',
+        name: this.state.textValue
+          })
+        }
+    )
+    } catch (err) {
+      console.log(err)
+    }
     this.setState(
       state => ({
         todos: state.todos.concat({ ticked: false, name: state.textValue })
@@ -27,17 +57,42 @@ class TodoList extends React.Component {
         this.setState({ textValue: '' })
       }
     )
+
   }
 
-  handleTick = idx => () => {
+  handleTick = idx => async () => {
+    // await fetch('http://localhost:3001/todos/'+ this.state.todos[idx].id, {
+    //   method: 'PATCH',
+    //   headers: {'content-type': 'application/json'},
+    //   body: JSON.stringify({
+    //     ticked: this.state.todos[idx].ticked
+    //       })
+    //     })
+
+    console.log(this.state.todos[idx].ticked)
     this.setState(state => ({
       todos: state.todos.map((todo, todoIdx) =>
         todoIdx === idx ? { ...todo, ticked: !todo.ticked } : todo
       )
     }))
+    
+    await fetch('http://localhost:3001/todos/'+ this.state.todos[idx].id, {
+      method: 'PATCH',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        ticked: !(this.state.todos[idx].ticked)
+          })
+        })
+  
   }
 
-  handleDelete = idx => () => {
+  handleDelete = idx => async() => {
+    console.log(this.state.todos[idx].id)
+    await fetch('http://localhost:3001/todos/' + this.state.todos[idx].id, {
+      method: 'DELETE',
+      headers: {'content-type': 'application/json'},
+    })
+
     this.setState(state => ({
       todos: state.todos.filter((todo, todoIdx) => todoIdx !== idx)
     }))
@@ -66,3 +121,8 @@ class TodoList extends React.Component {
 }
 
 export default TodoList
+
+
+
+
+ 
